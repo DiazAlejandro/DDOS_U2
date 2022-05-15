47797a9aa1a78267fc4730617c2c1c95e4dbe308
Alejandro Diaz <alejandro_diaz1253@hotmail.com>
2022-05-14 23:47:59 -0500

    Listar ventas

---
+++ servicio_venta_html/js/venta_get.js
@@ -0,0 +1,23 @@
+const venta_get = (() => {
+    const funcionExitoGet = (response) => {
+        for (let index = 0; index < response.data.length; index++) {
+            const idVenta = response.data[index].idVenta;
+            const fechaVenta = response.data[index].fechaVenta;
+            const nombreCliente = response.data[index].nombreCliente;
+            const producto = response.data[index].producto;
+            const cantidad = response.data[index].cantidad;
+            const precioUnitario = response.data[index].precioUnitario;
+            const precioTotal = response.data[index].precioTotal;
+            venta_elements.createRow(idVenta, fechaVenta, nombreCliente, producto, cantidad, precioUnitario, precioTotal);
+        }
+        console.log(response);
+    }
+
+    const funcionFallo = (err) => {
+        console.log(err);
+    }
+
+    cliente_http.get('http://localhost:8090/api/v1/venta/', funcionExitoGet, funcionFallo);
+
+})();
+
